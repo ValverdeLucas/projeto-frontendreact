5b06ed02822c91679c3805e8559cafe5752059dd
Lucas Valverde <valverdelucas95@gmail.com>
2023-11-16 20:06:22 -0300

    [configuração] Cria os componentes principais do projeto

---
+++ projeto-labecommerce/src/Components/ProductList/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductCard from '../ProductCard/ProductCard'
+
+function Home(){
+    return(
+        <>
+        <p>Home</p>
+        <ProductCard>
+        </ProductCard>
+        </>
+    )
+}
+
+export default Home
---
+++ projeto-labecommerce/src/Components/ShoppingCart/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Items from "../Items/Items.js"
+
+function Cart(){
+    return(
+        <>
+        <p>Cart</p>
+        <Items></Items>
+        </>
+    )
+}
+
+export default Cart
