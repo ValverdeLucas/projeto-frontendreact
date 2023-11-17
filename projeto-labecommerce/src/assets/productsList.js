c03e08050b7d0fd4a28f157526518767b6e54aa5
Lucas Valverde <valverdelucas95@gmail.com>
2023-11-17 19:06:23 -0300

    Inclussão do Assets com os Itens

---
+++ projeto-labecommerce/src/assets/productsList.js
@@ -0,0 +1,38 @@
+[
+    {
+      id: 1,
+      name: "Voyager 1",
+      value: 2500.0,
+      imageUrl: "https://picsum.photos/300/335?a=1"
+    },
+    {
+      id: 2,
+      name: "Space Shuttle Enterprise",
+      value: 5000.0,
+      imageUrl: "https://picsum.photos/300/335?a=2"
+    },
+    {
+      id: 3,
+      name: "Apollo I",
+      value: 500.0,
+      imageUrl: "https://picsum.photos/300/335?a=3"
+    },
+    {
+      id: 4,
+      name: "Soyuz",
+      value: 1500.0,
+      imageUrl: "https://picsum.photos/300/335?a=3"
+    },
+    {
+      id: 5,
+      name: "Space Shuttle Discovery",
+      value: 15000.0,
+      imageUrl: "https://picsum.photos/300/335?a=3"
+    },
+    {
+      id: 6,
+      name: "New Horizons",
+      value: 3500.0,
+      imageUrl: "https://picsum.photos/300/335?a=3"
+    }
+  ];
